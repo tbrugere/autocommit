fe6a2a03128955841bd5da16762416bfa929c4fd
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-12-19 18:20:42 +0100

    Update RAG database and tool descriptions, improve documentation

--- basic_rag/__init__.py
+++ basic_rag/__init__.py
@@ -1,3 +1,12 @@
+"""A simple RAG database
+
+A very simple RAG database that combines a faiss index and a SQLite database 
+to store chunks of text with embeddings.
+Embeddings are computed using the mistral API.
+
+This is adapted for storing a relatively low amount of chunks, in an on-disk store.
+"""
+
 from .basic_rag import RAGDatabase
 
 
--- mistral_tools/__init__.py
+++ mistral_tools/__init__.py
@@ -1,5 +1,5 @@
-"""
-High-level interface to the Mistral completion API
+"""High-level interface to the Mistral completion API
 
-Handles keeping track of state (e.g. the conversation with a model), automatically handling tool calls...
+Handles keeping track of state (e.g. the conversation with a model), 
+automatically handling tool calls, rate limiting...
 """
